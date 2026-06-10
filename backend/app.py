c7a904068f9dbf3f491a59f9dd8c85591682b75a
DEFSYDNEY <101777780+DEFSYDNEY@users.noreply.github.com>
2026-06-10 12:50:56 +1000

    Fixed issues but now 1 fail in stage 3

--- backend/app.py
+++ backend/app.py
@@ -83,7 +83,7 @@ def admin_auth_register():
 
     session_token, csrf_token = auth_register_user(email, password, name)
     # TODO: Include CSRF token in response 
-    response = jsonify({"csrf_token": csrf_token})
+    response = jsonify({"session_token": session_token, "csrf_token": csrf_token})
     response.set_cookie("session_token", session_token, httponly=True, samesite="Lax", secure=True)
     response.set_cookie("csrf_token", csrf_token, httponly=True, samesite="Lax", secure=True)
     return response, 200
@@ -100,7 +100,7 @@ def admin_auth_login():
 
     session_token, csrf_token = auth_login_user(email, password)
     # TODO: Include CSRF token in response
-    response = jsonify({"csrf_token": csrf_token})
+    response = jsonify({"session_token": session_token, "csrf_token": csrf_token})
     response.set_cookie("session_token", session_token, httponly=True, samesite="Lax", secure=True)
     response.set_cookie("csrf_token", csrf_token, httponly=True, samesite="Lax", secure=True)
 
