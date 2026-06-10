5c2101f5a2ab97533817100339023459435e0390
DEFSYDNEY <101777780+DEFSYDNEY@users.noreply.github.com>
2026-06-10 12:37:39 +1000

    Cookies not working like the specifications said so doing further research about cookies

--- backend/app.py
+++ backend/app.py
@@ -84,6 +84,8 @@ def admin_auth_register():
     session_token, csrf_token = auth_register_user(email, password, name)
     # TODO: Include CSRF token in response
     response = {"session_token": session_token, "csrf_token": csrf_token}
+    #response.set_cookie("session_token", session_token, httponly=True, samesite="Lax", secure=True)
+    #response.set_cookie("csrf_token", csrf_token, httponly=True, samesite="Lax", secure=True)
     return response, 200
 
 
@@ -99,6 +101,8 @@ def admin_auth_login():
     session_token, csrf_token = auth_login_user(email, password)
     # TODO: Include CSRF token in response
     response = {"session_token": session_token, "csrf_token": csrf_token}
+    #response.set_cookie("session_token", session_token, httponly=True, samesite="Lax", secure=True)
+    #response.set_cookie("csrf_token", csrf_token, httponly=True, samesite="Lax", secure=True)
     return response, 200
 
 
